ba7ed08b8ed29493dfbbe8767691a7391b4c162a
ymushet <egor.mushet@gmail.com>
2017-11-07 20:47:41 +0200

    help

---
+++ Rush00/cmake-build-debug/CMakeFiles/Rush00.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rush00.dir/src/game.cpp.o"
+  "CMakeFiles/Rush00.dir/src/Player.cpp.o"
+  "CMakeFiles/Rush00.dir/main.cpp.o"
+  "CMakeFiles/Rush00.dir/src/Enemy.cpp.o"
+  "CMakeFiles/Rush00.dir/src/Dragon.cpp.o"
+  "CMakeFiles/Rush00.dir/src/Ship.cpp.o"
+  "CMakeFiles/Rush00.dir/src/Meteor.cpp.o"
+  "Rush00.pdb"
+  "Rush00"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rush00.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Rush00/cmake-build-debug/CMakeFiles/Rush00.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Rush00.
+# This may be replaced when dependencies are built.
